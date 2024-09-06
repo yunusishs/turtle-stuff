14d07521209d41e1e1633177453ee61bfe0bbb2e
Yunus Ishs <109014525+yunusishs@users.noreply.github.com>
2024-09-06 19:34:20 +1000

    Update rgb triangle.py

--- rgb triangle.py
+++ rgb triangle.py
@@ -1,8 +1,9 @@
-import turtle as t
-t.colormode(255)
-t.shape('triangle')
-for i in range(255):
-    t.color(0,i,255-i)
-    t.shapesize((255-i)/2)
-    t.right(1)
-    t.stamp()
+from turtle import *
+colormode(255)
+shape('triangle')
+speed(0)
+for i in range(256):
+    color(255-i,255-i,255-i)
+    shapesize((256-i)/3)
+    right(1)
+    stamp()
