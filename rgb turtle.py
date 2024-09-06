c612acf013d3262e558573988a86aa6cdf2d5109
Yunus Ishs <109014525+yunusishs@users.noreply.github.com>
2024-09-06 19:40:00 +1000

    Update rgb turtle.py

--- rgb turtle.py
+++ rgb turtle.py
@@ -1,9 +1,9 @@
-import turtle as t
-t.colormode(255)
-t.speed('fastest')
-t.shape('turtle')
-for i in range(255):
-    t.color(i,0,255-i)
-    t.shapesize((255-i)/2)
-    t.right(1)
-    t.stamp()
+from turtle import *
+colormode(255)
+shape('turtle')
+speed(0)
+for i in range(256):
+    color(255-i,255-i,255-i)
+    shapesize((256-i)/2)
+    right(1)
+    stamp()
