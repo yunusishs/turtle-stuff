03fda805c446d98da15fc57f3d288c2606d37a8f
Yunus Ishs <109014525+yunusishs@users.noreply.github.com>
2024-09-06 19:33:06 +1000

    Update rgb square.py

--- rgb square.py
+++ rgb square.py
@@ -1,9 +1,9 @@
-import turtle as t
-t.colormode(255)
-t.shape('square')
-t.speed(0)
-for i in range(255):
-    t.color(255-i,255-i,255-i)
-    t.shapesize((255-i)/3)
-    t.right(1)
-    t.stamp()
+from turtle import *
+colormode(255)
+shape('square')
+speed(0)
+for i in range(256):
+    color(255-i,255-i,255-i)
+    shapesize((256-i)/3)
+    right(1)
+    stamp()
