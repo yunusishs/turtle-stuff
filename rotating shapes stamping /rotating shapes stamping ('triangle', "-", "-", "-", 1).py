ca41a57bf96b4476c722d363e3ec140b5e18e383
Yunus Ishs <109014525+yunusishs@users.noreply.github.com>
2024-09-08 17:07:32 +1000

    Delete rotating shapes stamping  directory

--- rotating shapes stamping /rotating shapes stamping ('triangle', "-", "-", "-", 1).py
+++
@@ -1,9 +0,0 @@
-from turtle import *
-colormode(255)
-shape('triangle')
-speed(0)
-for i in range(256):
-    color(255-i,255-i,255-i)
-    shapesize((256-i)/8)
-    right(1)
-    stamp()
--- rotating shapes stamping /rotating shapes stamping ('turtle', "-", "-", "-", 1).py
+++
@@ -1,9 +0,0 @@
-from turtle import *
-colormode(255)
-shape('turtle')
-speed(0)
-for i in range(256):
-    color(255-i,255-i,255-i)
-    shapesize((256-i)/8)
-    right(1)
-    stamp()
